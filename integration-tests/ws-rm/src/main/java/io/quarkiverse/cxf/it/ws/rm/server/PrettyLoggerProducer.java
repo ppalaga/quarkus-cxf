ea1d85244d216381f848657e09ed5c1500d24d9d
Freeman Fang <freeman.fang@gmail.com>
2023-08-31 11:12:56 -0400

    refactor ws-rm test

--- integration-tests/ws-rm/src/main/java/io/quarkiverse/cxf/it/ws/rm/server/PrettyLoggerProducer.java
+++ integration-tests/ws-rm/src/main/java/io/quarkiverse/cxf/it/ws/rm/server/PrettyLoggerProducer.java
@@ -4,6 +4,8 @@ import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Named;
 
 import org.apache.cxf.ext.logging.LoggingFeature;
+import org.apache.cxf.ws.rm.feature.RMFeature;
+import org.apache.cxf.ws.rmp.v200502.RMAssertion;
 
 public class PrettyLoggerProducer {
     @Produces
@@ -12,5 +14,23 @@ public class PrettyLoggerProducer {
         final LoggingFeature result = new LoggingFeature();
         result.setPrettyLogging(true);
         return result;
+    }
+
+    @Produces
+    @Named
+    RMFeature rmFeature() {
+        org.apache.cxf.ws.rm.feature.RMFeature rmFeature = new org.apache.cxf.ws.rm.feature.RMFeature();
+        RMAssertion.BaseRetransmissionInterval baseRetransmissionInterval = new RMAssertion.BaseRetransmissionInterval();
+        baseRetransmissionInterval.setMilliseconds(Long.valueOf(60000));
+        RMAssertion.AcknowledgementInterval acknowledgementInterval = new RMAssertion.AcknowledgementInterval();
+        acknowledgementInterval.setMilliseconds(Long.valueOf(2000));
+
+        RMAssertion rmAssertion = new RMAssertion();
+        rmAssertion.setAcknowledgementInterval(acknowledgementInterval);
+        rmAssertion.setBaseRetransmissionInterval(baseRetransmissionInterval);
+
+        rmFeature.setRMAssertion(rmAssertion);
+
+        return rmFeature;
     }
 }
--- integration-tests/ws-rm/src/main/java/io/quarkiverse/cxf/it/ws/rm/server/WsrmHelloService.java
+++ integration-tests/ws-rm/src/main/java/io/quarkiverse/cxf/it/ws/rm/server/WsrmHelloService.java
@@ -2,13 +2,10 @@ package io.quarkiverse.cxf.it.ws.rm.server;
 
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
-import jakarta.xml.ws.soap.Addressing;
-
-import org.apache.cxf.interceptor.InInterceptors;
 
 @WebService(targetNamespace = "https://quarkiverse.github.io/quarkiverse-docs/quarkus-cxf/test/ws-rm")
-@Addressing(required = true)
-@InInterceptors(interceptors = { "io.quarkiverse.cxf.it.ws.rm.server.RMStoreCheckInterceptor" })
+//@Addressing(required = true)
+//@InInterceptors(interceptors = { "io.quarkiverse.cxf.it.ws.rm.server.RMStoreCheckInterceptor" })
 public interface WsrmHelloService {
     @WebMethod
     String sayHello(String name);
