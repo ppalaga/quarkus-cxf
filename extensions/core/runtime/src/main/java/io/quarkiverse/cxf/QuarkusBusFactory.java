7eff5563a58ea5fbff111db0d746e656688e3abd
Peter Palaga <ppalaga@redhat.com>
2026-01-13 20:07:15 +0100

    i2001

--- extensions/core/runtime/src/main/java/io/quarkiverse/cxf/QuarkusBusFactory.java
+++ extensions/core/runtime/src/main/java/io/quarkiverse/cxf/QuarkusBusFactory.java
@@ -40,7 +40,8 @@ public class QuarkusBusFactory extends CXFBusFactory {
         bus.setExtension(new WrapperHelperClassLoader(bus), WrapperHelperCreator.class);
         bus.setExtension(new ExtensionClassLoader(bus), ExtensionClassCreator.class);
         bus.setExtension(new ExceptionClassLoader(bus), ExceptionClassCreator.class);
-        bus.setExtension(new WrapperClassLoader(bus), WrapperClassCreator.class);
+        bus.setExtension(new QuarkusWrapperClassLoader(bus), WrapperClassCreator.class);
+        //bus.setExtension(new WrapperClassLoader(bus), WrapperClassCreator.class);
         bus.setExtension(new FactoryClassLoader(bus), FactoryClassCreator.class);
         bus.setExtension(new GeneratedNamespaceClassLoader(bus), NamespaceClassCreator.class);
         bus.setExtension(new ClassLoaderProxyService(new GeneratedNamespaceClassLoader(bus)), ClassLoaderService.class);
@@ -52,6 +53,22 @@ public class QuarkusBusFactory extends CXFBusFactory {
      */
     static void addBusCustomizer(Consumer<Bus> customizer) {
         customizers.add(customizer);
+    }
+
+    public static class QuarkusWrapperClassLoader extends WrapperClassLoader {
+        public QuarkusWrapperClassLoader(Bus bus) {
+            super(bus);
+        }
+
+        @Override
+        protected Class<?> findClass(String className, Class<?> callingClass) {
+            try {
+                return Class.forName(className, true, Thread.currentThread().getContextClassLoader());
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException("Could not load " + className, e);
+            }
+        }
+
     }
 
 }
